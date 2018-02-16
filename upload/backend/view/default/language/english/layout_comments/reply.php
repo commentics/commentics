8343fbc8e5849395525a11ba0152184b264f4f54
Steven <support@commentics.org>
2018-02-16 20:07:45 +0000

    Updating hint lang

--- upload/backend/view/default/language/english/layout_comments/reply.php
+++ upload/backend/view/default/language/english/layout_comments/reply.php
@@ -18,5 +18,5 @@ $_['lang_hint_enabled']			= 'Whether to allow the user to reply to each comment.
 $_['lang_hint_hide_replies']	= 'Whether to initially hide the replies with a link to show them.';
 $_['lang_hint_reply_indent']	= 'How many pixels should each reply be indented by.';
 $_['lang_hint_reply_depth']		= 'How many levels of replies before the reply link is hidden. Enter a number of 1 or higher. Make sure that the width of the replies can fit on your page.';
-$_['lang_hint_scroll_reply']	= 'Gradually scroll down to the form after clicking the reply link.';
+$_['lang_hint_scroll_reply']	= 'Gradually scroll up to the form after clicking the reply link.';
 ?>
