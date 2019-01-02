9db01d590bd7369188ad097857550bae04ddd7bf
Steven <support@commentics.org>
2019-01-02 19:40:30 +0000

    Config file indentation

--- upload/install/model/main/database.php
+++ upload/install/model/main/database.php
@@ -5,16 +5,17 @@ class MainDatabaseModel extends Model
 {
     public function writeConfig()
     {
-        $data = '<?php
-        /* Database Details */
-        define(\'CMTX_DB_DATABASE\', \'' . addslashes($this->request->post['database']) . '\');
-        define(\'CMTX_DB_USERNAME\', \'' . addslashes($this->request->post['username']) . '\');
-        define(\'CMTX_DB_PASSWORD\', \'' . addslashes($this->request->post['password']) . '\');
-        define(\'CMTX_DB_HOSTNAME\', \'' . addslashes($this->request->post['hostname']) . '\');
-        define(\'CMTX_DB_PORT\', \'' . addslashes($this->request->post['port']) . '\');
-        define(\'CMTX_DB_PREFIX\', \'' . addslashes($this->request->post['prefix']) . '\');
-        define(\'CMTX_DB_DRIVER\', \'' . addslashes($this->request->post['driver']) . '\');
-        ';
+        $data = '';
+
+        $data .= '<?php' . PHP_EOL;
+        $data .= '/* Database Details */' . PHP_EOL;
+        $data .= 'define(\'CMTX_DB_DATABASE\', \'' . addslashes($this->request->post['database']) . '\');' . PHP_EOL;
+        $data .= 'define(\'CMTX_DB_USERNAME\', \'' . addslashes($this->request->post['username']) . '\');' . PHP_EOL;
+        $data .= 'define(\'CMTX_DB_PASSWORD\', \'' . addslashes($this->request->post['password']) . '\');' . PHP_EOL;
+        $data .= 'define(\'CMTX_DB_HOSTNAME\', \'' . addslashes($this->request->post['hostname']) . '\');' . PHP_EOL;
+        $data .= 'define(\'CMTX_DB_PORT\', \'' . addslashes($this->request->post['port']) . '\');' . PHP_EOL;
+        $data .= 'define(\'CMTX_DB_PREFIX\', \'' . addslashes($this->request->post['prefix']) . '\');' . PHP_EOL;
+        $data .= 'define(\'CMTX_DB_DRIVER\', \'' . addslashes($this->request->post['driver']) . '\');' . PHP_EOL;
 
         $handle = fopen('../config.php', 'w');
 
