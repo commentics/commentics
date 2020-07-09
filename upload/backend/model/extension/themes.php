3991f2f0c775a9fde26d69e4b7a0a0e1894c0904
Steven <support@commentics.org>
2020-07-09 19:08:44 +0100

    Clear template cache when changing theme

--- upload/backend/model/extension/themes.php
+++ upload/backend/model/extension/themes.php
@@ -21,6 +21,11 @@ class ExtensionThemesModel extends Model
         if ((isset($data['optimize']) && !$this->setting->get('optimize')) || (!isset($data['optimize']) && $this->setting->get('optimize'))) {
             remove_directory(CMTX_DIR_CACHE . 'template/', false);
         }
+
+        /* If frontend theme setting has changed, clear template cache so the new theme's template files are used */
+        if ($data['theme_frontend'] != $this->setting->get('theme_frontend')) {
+            remove_directory(CMTX_DIR_CACHE . 'template/', false);
+        }
     }
 
     public function getFrontendThemes()
