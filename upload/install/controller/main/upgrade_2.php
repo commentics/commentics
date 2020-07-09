c7678c4d0c6cc5a51be6984beeea95e20f2ae718
Steven <support@commentics.org>
2020-07-09 18:34:46 +0100

    Allow direct upgrade from v2.5 and above

--- upload/install/controller/main/upgrade_2.php
+++ upload/install/controller/main/upgrade_2.php
@@ -24,18 +24,33 @@ class MainUpgrade2Controller extends Controller
                     break;
                 case '3.3':
                     $this->model_main_upgrade_2->upgrade('3.3 -> 3.4');
+                    $this->model_main_upgrade_2->upgrade('3.4 -> 4.0');
                     break;
                 case '3.2':
                     $this->model_main_upgrade_2->upgrade('3.2 -> 3.3');
+                    $this->model_main_upgrade_2->upgrade('3.3 -> 3.4');
+                    $this->model_main_upgrade_2->upgrade('3.4 -> 4.0');
                     break;
                 case '3.1':
                     $this->model_main_upgrade_2->upgrade('3.1 -> 3.2');
+                    $this->model_main_upgrade_2->upgrade('3.2 -> 3.3');
+                    $this->model_main_upgrade_2->upgrade('3.3 -> 3.4');
+                    $this->model_main_upgrade_2->upgrade('3.4 -> 4.0');
                     break;
                 case '3.0':
                     $this->model_main_upgrade_2->upgrade('3.0 -> 3.1');
+                    $this->model_main_upgrade_2->upgrade('3.1 -> 3.2');
+                    $this->model_main_upgrade_2->upgrade('3.2 -> 3.3');
+                    $this->model_main_upgrade_2->upgrade('3.3 -> 3.4');
+                    $this->model_main_upgrade_2->upgrade('3.4 -> 4.0');
                     break;
                 case '2.5':
                     $this->model_main_upgrade_2->upgrade('2.5 -> 3.0');
+                    $this->model_main_upgrade_2->upgrade('3.0 -> 3.1');
+                    $this->model_main_upgrade_2->upgrade('3.1 -> 3.2');
+                    $this->model_main_upgrade_2->upgrade('3.2 -> 3.3');
+                    $this->model_main_upgrade_2->upgrade('3.3 -> 3.4');
+                    $this->model_main_upgrade_2->upgrade('3.4 -> 4.0');
                     break;
                 default:
                     $valid_upgrade = false;
