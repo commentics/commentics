02936ba8d8b216a3e6648f580d63061c9264d5a5
Steven <support@commentics.com>
2021-04-26 19:32:54 +0100

    Change folder in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var uglify = require('gulp-uglify');
 
 // Lint backend Sass
 gulp.task('lint-backend', function() {
-    return gulp.src('upload/backend/view/default/stylesheet/sass/stylesheet.scss')
+    return gulp.src('commentics/backend/view/default/stylesheet/sass/stylesheet.scss')
         .pipe(stylelint({
             reporters: [
                 {formatter: 'string', console: true}
@@ -19,7 +19,7 @@ gulp.task('lint-backend', function() {
 
 // Lint frontend Sass
 gulp.task('lint-frontend', function() {
-    return gulp.src('upload/frontend/view/default/stylesheet/sass/partial/*.scss')
+    return gulp.src('commentics/frontend/view/default/stylesheet/sass/partial/*.scss')
         .pipe(stylelint({
             reporters: [
                 {formatter: 'string', console: true}
@@ -29,7 +29,7 @@ gulp.task('lint-frontend', function() {
 
 // Lint install Sass
 gulp.task('lint-install', function() {
-    return gulp.src('upload/install/view/default/stylesheet/sass/stylesheet.scss')
+    return gulp.src('commentics/install/view/default/stylesheet/sass/stylesheet.scss')
         .pipe(stylelint({
             reporters: [
                 {formatter: 'string', console: true}
@@ -43,71 +43,71 @@ var autoprefixOptions = {
 
 /* Precompile and minify Sass for backend */
 gulp.task('sass-backend', function() {
-    return gulp.src('upload/backend/view/default/stylesheet/sass/stylesheet.scss')
+    return gulp.src('commentics/backend/view/default/stylesheet/sass/stylesheet.scss')
         .pipe(sass({outputStyle: 'expanded'}))
         .pipe(autoprefix(autoprefixOptions))
-        .pipe(gulp.dest('upload/backend/view/default/stylesheet/css'))
+        .pipe(gulp.dest('commentics/backend/view/default/stylesheet/css'))
         .pipe(rename('stylesheet.min.css'))
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('upload/backend/view/default/stylesheet/css'))
+        .pipe(gulp.dest('commentics/backend/view/default/stylesheet/css'))
 });
 
 /* Precompile and minify Sass for frontend */
 gulp.task('sass-frontend', function() {
-    return gulp.src('upload/frontend/view/default/stylesheet/sass/stylesheet.scss')
+    return gulp.src('commentics/frontend/view/default/stylesheet/sass/stylesheet.scss')
         .pipe(sass({outputStyle: 'expanded'}))
         .pipe(autoprefix(autoprefixOptions))
-        .pipe(gulp.dest('upload/frontend/view/default/stylesheet/css'))
+        .pipe(gulp.dest('commentics/frontend/view/default/stylesheet/css'))
         .pipe(rename('stylesheet.min.css'))
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('upload/frontend/view/default/stylesheet/css'))
+        .pipe(gulp.dest('commentics/frontend/view/default/stylesheet/css'))
 });
 
 /* Precompile and minify Sass for install */
 gulp.task('sass-install', function() {
-    return gulp.src('upload/install/view/default/stylesheet/sass/stylesheet.scss')
+    return gulp.src('commentics/install/view/default/stylesheet/sass/stylesheet.scss')
         .pipe(sass({outputStyle: 'expanded'}))
         .pipe(autoprefix(autoprefixOptions))
-        .pipe(gulp.dest('upload/install/view/default/stylesheet/css'))
+        .pipe(gulp.dest('commentics/install/view/default/stylesheet/css'))
         .pipe(rename('stylesheet.min.css'))
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('upload/install/view/default/stylesheet/css'))
+        .pipe(gulp.dest('commentics/install/view/default/stylesheet/css'))
 });
 
 var js_files = [
-    'upload/frontend/view/default/javascript/autodetect.js',
-    'upload/3rdparty/highlight/highlight.js',
-    'upload/3rdparty/timeago/timeago.js',
-    'upload/frontend/view/default/javascript/common.js'
+    'commentics/frontend/view/default/javascript/autodetect.js',
+    'commentics/3rdparty/highlight/highlight.js',
+    'commentics/3rdparty/timeago/timeago.js',
+    'commentics/frontend/view/default/javascript/common.js'
 ];
 
 /* Concatenate and minify JS */
 gulp.task('common', function() {
     return gulp.src(js_files)
         .pipe(concat('common.min.js'))
-        .pipe(gulp.dest('upload/frontend/view/default/javascript'))
+        .pipe(gulp.dest('commentics/frontend/view/default/javascript'))
         .pipe(uglify())
-        .pipe(gulp.dest('upload/frontend/view/default/javascript'));
+        .pipe(gulp.dest('commentics/frontend/view/default/javascript'));
 });
 
 var jquery = [
-    'upload/frontend/view/default/javascript/jquery/jquery.min.js'
+    'commentics/frontend/view/default/javascript/jquery/jquery.min.js'
 ];
 
 /* Concatenate and minify JS (with jQuery) */
 gulp.task('common-jq', function() {
     return gulp.src(jquery.concat(js_files))
         .pipe(concat('common-jq.min.js'))
-        .pipe(gulp.dest('upload/frontend/view/default/javascript'))
+        .pipe(gulp.dest('commentics/frontend/view/default/javascript'))
         .pipe(uglify())
-        .pipe(gulp.dest('upload/frontend/view/default/javascript'));
+        .pipe(gulp.dest('commentics/frontend/view/default/javascript'));
 });
 
 /* Auto precompile Sass on save */
 gulp.task('watch', function() {
-    gulp.watch('upload/backend/view/default/stylesheet/sass/**/*.scss', ['sass-backend']);
-    gulp.watch('upload/frontend/view/default/stylesheet/sass/**/*.scss', ['sass-frontend']);
-    gulp.watch('upload/install/view/default/stylesheet/sass/**/*.scss', ['sass-install']);
+    gulp.watch('commentics/backend/view/default/stylesheet/sass/**/*.scss', ['sass-backend']);
+    gulp.watch('commentics/frontend/view/default/stylesheet/sass/**/*.scss', ['sass-frontend']);
+    gulp.watch('commentics/install/view/default/stylesheet/sass/**/*.scss', ['sass-install']);
 });
 
 gulp.task('lint', gulp.series('lint-backend', 'lint-frontend', 'lint-install'));
