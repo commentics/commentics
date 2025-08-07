2756f56d50c72d289ef19a78d9d4f9942a0834aa
Steven <support@commentics.com>
2025-08-07 19:48:32 +0100

    Add no frontend message

--- comments/index.php
+++ comments/index.php
@@ -1,21 +1,28 @@
 <?php
 if (file_exists('install/')) {
     header('Location: ' . 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/install/');
-
-    die();
+    exit;
 } else {
     header('HTTP/1.0 403 Forbidden');
-
-    echo '<!DOCTYPE html>';
-    echo '<html>';
-    echo '<head>';
-    echo '<title>403 Forbidden</title>';
-    echo '<meta name="robots" content="noindex">';
-    echo '</head>';
-    echo '<body>';
-    echo '<h1>Forbidden</h1>';
-    echo '<p>You don\'t have permission to access this folder.</p>';
-    echo '<hr>';
-    echo '</body>';
-    echo '</html>';
+    header('Content-Type: text/html; charset=UTF-8');
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Commentics</title>
+        <meta charset="UTF-8">
+        <meta name="robots" content="noindex">
+        <link rel="stylesheet" href="403.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="content">
+                <p class="heading">Commentics</p>
+                <p class="message">There's nothing to see here</p>
+                <p class="link"><a href="https://commentics.com/faq/general/no-frontend" target="_blank">Learn More</a></p>
+            </div>
+        </div>
+    </body>
+    </html>
+    <?php
 }
